d7423d706e2c684207767ab277c16d104328b5eb
shouye001 <112637521+shouye001@users.noreply.github.com>
2022-09-01 22:04:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,35 +4,35 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wxd9ab98c804b2b53d",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "e5cbed253bc2e497d831c8691e56196a",
 
     // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    TEMPLATE_ID: "4VNRJJw0XzA4d0hGVBmKuq8vFZI6LdLo590B2W64xzc",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    CALLBACK_TEMPLATE_ID: "4VNRJJw0XzA4d0hGVBmKuq8vFZI6LdLo590B2W64xzc",
 
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     // [{
     //   name: "张三",
-    //   id: "wxafasdasdd-zxfvsdfd"
+    //   id: "4VNRJJw0XzA4d0hGVBmKuq8vFZI6LdLo590B2W64xzc"
     // },
     // {
     //   name: "李四",
-    //   id: "wxafasdasdd-zxfvsdfd"
+    //   id: "4VNRJJw0XzA4d0hGVBmKuq8vFZI6LdLo590B2W64xzc"
     // }]
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
+      {name: '张大小姐', id: "o1cxB503uatuwpdwG43qHljFi2D4"}, 
       {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
       {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
       {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
+      {name: '自己',id: "o1cxB503uatuwpdwG43qHljFi2D4"}, 
     ],
      
     /**
@@ -42,9 +42,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "江苏",
     // 所在城市
-    CITY: "惠州",
+    CITY: "南京",
 
     /** 重要节日相关 */
 
@@ -53,7 +53,7 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
+      {"type": "生日", "name": "张大小姐", "year": "1999", "date": "09-09"},
       {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
       {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
       {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
